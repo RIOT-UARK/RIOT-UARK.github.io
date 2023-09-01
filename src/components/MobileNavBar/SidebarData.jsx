297861f6db1ebaa2388abe5bec5f4970fe06d97a
BakedPotato101 <jackson.baker7562@gmail.com>
2023-09-01 00:53:54 -0500

    more routing

--- src/components/MobileNavBar/SidebarData.jsx
+++ src/components/MobileNavBar/SidebarData.jsx
@@ -3,32 +3,32 @@ import React from 'react';
 export const SidebarData = [
     {
         title: 'Home',
-        path: '/',
+        path: '/riot-website-/',
         cName: 'nav-text',
     },
     {
         title: 'Microbots',
-        path: '/Microbots',
+        path: '/riot-website-/Microbots',
         cName: 'nav-text',
     },
     {
         title: 'Projects',
-        path: '/Projects',
+        path: '/riot-website-/Projects',
         cName: 'nav-text',
     },
     {
         title: 'Meet the Team',
-        path: '/MeetTheTeam',
+        path: '/riot-website-/MeetTheTeam',
         cName: 'nav-text',
     },
     {
         title: 'Donate',
-        path: '/Donate',
+        path: '/riot-website-/Donate',
         cName: 'nav-text',
     },
     {
         title: 'Getting Started',
-        path: '/GettingStarted',
+        path: '/riot-website-/GettingStarted',
         cName: 'nav-text',
     },
 ];
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/Microbots">Microbots</Link>
-            <Link to="/Projects">Projects</Link>
-            <Link to="/MeetTheTeam">Meet the Team</Link>
-            <Link to="/Donate">Donate</Link>
-            <Link to="/GettingStarted">Getting Started</Link>
+            <Link to="/riot-website-/">Home</Link>
+            <Link to="/riot-website-/Microbots">Microbots</Link>
+            <Link to="/riot-website-/Projects">Projects</Link>
+            <Link to="/riot-website-/MeetTheTeam">Meet the Team</Link>
+            <Link to="/riot-website-/Donate">Donate</Link>
+            <Link to="/riot-website-/GettingStarted">Getting Started</Link>
             <DarkModeToggle />
         </nav>
     );
