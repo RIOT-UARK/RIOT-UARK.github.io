a61849568bfbfcfcb525c05ee3b80bfc7855046b
BakedPotato101 <jackson.baker7562@gmail.com>
2023-09-29 15:22:13 -0500

    cleaned up footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,35 @@
 import { BsDiscord as Discord, BsInstagram as Instagram, BsTwitter as Twitter } from 'react-icons/bs';
-const footer = () => {
+
+const Footer = () => {
     return (
         <footer>
+            <div>
+                <strong>Contact Us:</strong> robotics@uark.edu
+            </div>
+            <div style={{ margin: '10px 0' }}>
+                <span>Nathaniel House (President): nah007@uark.edu | </span>
+                <span>Grace Harding (Vice President): gharding@uark.edu</span>
+            </div>
             <center>
-                <p className="font-16">Contact Us: robotics@uark.edu</p>
-                <p className="font-16">Nathaniel House (President): nah007@uark.edu</p>
-                <p className="font-16">Grace Harding (Vice President): gharding@uark.edu</p>
                 <div className="socialMediaContainer">
-                    <a className="socialMediaLink" href="https://discord.gg/5YRM3pQYev">
-                        <Discord size={75} color="white" />
-                        <strong className="socialMediaText">Join our Discord</strong>
+                    <a href="https://discord.gg/5YRM3pQYev" title="Join our Discord">
+                        <Discord size={40} color={"#ecf3fe"} />
                     </a>
-                    <a className="socialMediaLink" href="https://www.instagram.com/riot_uark/">
-                        <Instagram size={75} color="white" />
-                        <strong className="socialMediaText">Follow us on Instagram</strong>
+                    <a href="https://www.instagram.com/riot_uark/" title="Follow us on Instagram">
+                        <Instagram size={40} color={"#ecf3fe"}/>
                     </a>
-                    <a className="socialMediaLink" href="https://twitter.com/riot_uark">
-                        <Twitter size={75} color="white" />
-                        <strong className="socialMediaText">Follow us on Twitter</strong>
+                    <a href="https://twitter.com/riot_uark" title="Follow us on Twitter">
+                        <Twitter size={40} color={"#ecf3fe"}/>
                     </a>
                 </div>
-                <p className="font-12">Powered by Supabase</p>
-                <p className="font-12">RIOT 2023, authored by Ben Fletcher and Jackson Baker</p>
             </center>
+            
+            <div style={{ fontSize: '12px', marginTop: '10px' }}>
+                <p>Powered by Supabase</p>
+                <p>RIOT 2023, authored by Ben Fletcher and Jackson Baker</p>
+            </div>
         </footer>
     );
 };
-export default footer;
+
+export default Footer;
