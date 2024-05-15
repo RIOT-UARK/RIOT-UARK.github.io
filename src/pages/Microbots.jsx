e547f1f760ef31865af5a3e5326fa09f0033fa4c
Ben Fletcher <baf007@uark.edu>
2024-05-15 18:35:46 -0500

    Removed placholder stuff and edited text blurbs

--- src/pages/Microbots.jsx
+++ src/pages/Microbots.jsx
@@ -21,7 +21,7 @@ function ToggleSignup({ offSeason, formLink }) {
 const Microbots = () => {
     const [Battlebots, setBattlebots] = useState(null);
     const [openPopupId, setOpenPopupId] = useState(null); // State to track open popup ID
-    const prevSemesters = ['Fall 2022', 'Spring 2023', 'Fall 2023']; //Temporary hardcoded array
+    const prevSemesters = ['Fall 2022', 'Spring 2023']; //Temporary hardcoded array. Add Fall 2023 when we get the list of bots that competed
 
     ////////////////////////////////////////////////////
     //Manually change these as needed
@@ -53,13 +53,13 @@ const Microbots = () => {
                 <p>
                     As part of RIOT's vision, every year, we host a microbots competition where students new to robotics
                     pilot small robots in a variety of different competitive events. We've hosted each previous semester
-                    two separate battle bot tournaments where students build a plastic, 3D printed, remote controlled
-                    battle bot in a competition to fight and survive against another designs. Our winners receive
-                    special awards as the crowned champion of the microbots tournament each time a competition is
+                    two separate battle bot tournaments where students build a plastic, 3D printed, remote-controlled
+                    battlebot in a competition to fight and survive against other designs. Our winners receive
+                    special awards such as the crowned champion of the microbots tournament each time a competition is
                     hosted. Each fall semester we host an internal competition where we set the fight in the University
-                    of Arkansas Union for the student division to recruit additional interested students into the fold
-                    of our organization. In the spring, we host a community event at the Fayetteville public library,
-                    where students demonstrate their designs in front of a live audience and compete for trophies the
+                    of Arkansas Student Union to recruit additional interested students into the fold
+                    of our organization. In the spring, we host a community event at the Fayetteville Public Library,
+                    where students demonstrate their designs in front of a live audience and compete for trophies like the
                     trophies of champion, best design, crowd favorite, and best driver.
                 </p>
             </div>
