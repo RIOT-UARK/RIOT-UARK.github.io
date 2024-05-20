be75f70c46644036f502dc32667c5a8177acc1d9
BakedPotato101 <jackson.baker7562@gmail.com>
2024-05-19 22:11:29 -0500

    fixed more conflictions with microbots page

--- src/pages/Microbots.jsx
+++ src/pages/Microbots.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect } from 'react'
-import { fetchMembers } from '../utils/fetchMembers'
-
-import BattlebotCard from '../components/battlebotCard/battlebotCard';
-
-const Microbots = () => {
-    
-    const currentSemester = 'Fall 2023';
-    const offSeason = true;
-    const formLink = 'https://www.youtube.com/@Ayden3D';
-
-=======
 import { useEffect, useState } from 'react';
 import { fetchBattlebots } from '../db/db';
 
@@ -53,7 +40,6 @@ const Microbots = () => {
         };
         fetchBattlebotsData();
     }, []);
->>>>>>> main
 
     // Function to toggle popup state and set open popup ID
     const handleTogglePopup = (semester) => {
@@ -67,15 +53,9 @@ const Microbots = () => {
                 <p>
                     As part of RIOT's vision, every year, we host a microbots competition where students new to robotics
                     pilot small robots in a variety of different competitive events. We've hosted each previous semester
-<<<<<<< HEAD
-                    two separate battle bot tournaments where students build a plastic, 3D printed, remote controlled
-                    battle bot in a competition to fight and survive against another designs. Our winners receive
-                    special awards as the crowned champion of the microbots tournament each time a competition is
-=======
                     two separate battle bot tournaments where students build a plastic, 3D printed, remote-controlled
                     battlebot in a competition to fight and survive against other designs. Our winners receive
                     special awards such as the crowned champion of the microbots tournament each time a competition is
->>>>>>> main
                     hosted. Each fall semester we host an internal competition where we set the fight in the University
                     of Arkansas Student Union to recruit additional interested students into the fold
                     of our organization. In the spring, we host a community event at the Fayetteville Public Library,
@@ -92,23 +72,11 @@ const Microbots = () => {
             </center>
             <div>
                 <center>
-                    {/*<ToggleSignup key={offSeason} offSeason={offSeason} formLink={formLink} />*/}
+                    <ToggleSignup key={offSeason} offSeason={offSeason} formLink={formLink} />
                 </center>
             </div>
             <center>
                 <div>
-<<<<<<< HEAD
-                    <h2>How to Support</h2>
-                    <p>
-                        To support the microbots competition, or fund a specific student's robot, please contact us at
-                        robotics@uark.edu
-                    </p>
-                </div>
-                <div>
-                    <h2>Questions?</h2>
-                </div>
-            </center>
-=======
                     <h2>Want to support us or have questions?</h2>
                     <p>Please contact robotics@uark.edu</p>
                 </div>
@@ -149,7 +117,6 @@ const Microbots = () => {
                     </div>
                 </center>
             </div>
->>>>>>> main
         </div>
     );
 };
